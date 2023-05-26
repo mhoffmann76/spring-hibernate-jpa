ee03218e8b2a851f039ae119072f0313da4e1b92
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-05-26 11:51:37 -0300

    Realizado tratamento de erros

--- src/main/java/com/educaweb/course/resources/UserResource.java
+++ src/main/java/com/educaweb/course/resources/UserResource.java
@@ -3,8 +3,6 @@ package com.educaweb.course.resources;
 import com.educaweb.course.entities.User;
 import com.educaweb.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -28,20 +26,19 @@ public class UserResource {
 
     @GetMapping(value = "/{id}")
 
-    public ResponseEntity<?> findById(@PathVariable Long id) {
-        try {
-            User obj = service.findById(id);
-            return ResponseEntity.ok().body(obj);
+    public ResponseEntity<User> findById(@PathVariable Long id) {
 
-        } catch (NoSuchElementException e) {
-            String errorMessage = "User not found with ID: " + id;
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
-        } catch (Exception e) {
-            String errorMessage = "Internal server error occurred";
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+
+
+
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
 
 
     }
 
+
 }
+
+
