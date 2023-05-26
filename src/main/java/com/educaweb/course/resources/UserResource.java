e5a79a979f9953120ae6595a03da334ececd59a6
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-05-25 22:26:00 -0300

    Service layer, component registration

--- src/main/java/com/educaweb/course/resources/UserResource.java
+++ src/main/java/com/educaweb/course/resources/UserResource.java
@@ -1,18 +1,47 @@
 package com.educaweb.course.resources;
 
 import com.educaweb.course.entities.User;
+import com.educaweb.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Maria", "maria@gmail.com", "27 9999-0909","12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        try {
+            User obj = service.findById(id);
+            return ResponseEntity.ok().body(obj);
+
+        } catch (NoSuchElementException e) {
+            String errorMessage = "User not found with ID: " + id;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        } catch (Exception e) {
+            String errorMessage = "Internal server error occurred";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+
     }
 
 }
