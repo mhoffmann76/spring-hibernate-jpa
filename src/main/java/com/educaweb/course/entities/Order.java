f1068617bb74e8aec2ce369272b5d8fbc0a5341a
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-05-29 23:58:05 -0300

    order status num

--- src/main/java/com/educaweb/course/entities/Order.java
+++ src/main/java/com/educaweb/course/entities/Order.java
@@ -1,5 +1,7 @@
 package com.educaweb.course.entities;
 
+import com.educaweb.course.entities.enums.OrderStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -16,7 +18,10 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T' HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
 
 
     @ManyToOne
@@ -27,10 +32,11 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
 
     }
@@ -49,6 +55,20 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(String.valueOf(orderStatus));
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+
+            this.orderStatus = orderStatus.getCode();
+
+        }
+
     }
 
     public User getClient() {
